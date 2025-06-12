8d6663baebda98b6471f8e64695e116c36136cff
rahucode1 <rahucode1@outlook.com>
2025-06-12 14:29:47 +0530

    added this page to test SSG

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function AboutPage() {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+                padding: '2rem',
+            }}
+        >
+            <h1>About Us</h1>
+            <p>This is a simple Next.js application demonstrating various features.</p>
+            <p>Feel free to explore the code and learn more!</p>
+        </div>
+    );
+}
+//This is will be generated at build time and then served as static HTML
